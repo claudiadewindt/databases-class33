64bb6e2ae3605fd886dfb70221b9d5b2c4414399
Claudia <caucrah@gmail.com>
2021-10-20 13:58:05 +0200

    Fixed the homework

--- Week2/homework/app.js
+++ Week2/homework/app.js
@@ -1,79 +1,69 @@
 const mysql = require('mysql');
 const util = require('util');
-
-const {
-  CREATE_TABLE_AUTHORS,
-  ADD_MENTOR_COLUMN,
-  CREATE_TABLE_RESEARCH_PAPERS,
-  CREATE_TABLE_AUTHORS_RESEARCH_PAPERS,
-  AUTHORS_AND_THERE_MENTORS,
-  AUTHORS_INFORMATION_AND_PAPERS_TITLE,
-  RESEARCHES_AND_NUMBER_OF_AUTHORS,
-  NUMBER_OF_PAPERS_WRITTEN_BY_FEMALES,
-  AVERAGE_INDEX_BY_UNIVERSITY,
-  PAPERS_BY_UNIVERSITY,
-  MIN_MAX_INDEX_BY_UNIVERSITY,
-} = require('./query');
-
+const queries = require('./query');
 const { authors, research_Papers, author_papers } = require('./data');
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
-  multipleStatements: true,
 });
 
-// node native promisify
-const executeQuery = util.promisify(connection.query.bind(connection));
+const execQuery = util.promisify(connection.query.bind(connection));
 
-(async () => {
-  const checkDatabase = (database) => {
-    return `DROP DATABASE IF EXISTS ${database}`;
-  };
-  const createDatabase = `CREATE DATABASE IF NOT EXISTS papers;
-   USE papers;`;
-
+const seedDatabase = async () => {
   connection.connect();
 
   try {
-    await Promise.all([
-      executeQuery(checkDatabase('papers')),
-      executeQuery(createDatabase),
-    ]);
-    console.log('Database is created');
+    await execQuery(`DROP DATABASE IF EXISTS papers;`);
+    await execQuery(`CREATE DATABASE papers;`);
+    await execQuery(`USE papers;`);
 
-    await Promise.all([
-      (executeQuery(CREATE_TABLE_AUTHORS),
-      executeQuery(CREATE_TABLE_RESEARCH_PAPERS)),
-      executeQuery(ADD_MENTOR_COLUMN),
-      executeQuery(CREATE_TABLE_AUTHORS_RESEARCH_PAPERS),
-    ]);
+    // Exercise 1: Keys
+    await execQuery(queries.authors.table_creation);
+    await execQuery(queries.authors.add_mentor);
 
-    authors.forEach(async (author) => {
-      await executeQuery('INSERT INTO authors SET ?', author);
-    });
-    research_Papers.forEach(async (research) => {
-      await executeQuery('INSERT INTO research_papers SET ?', research);
-    });
-    author_papers.forEach(async (author_paper) => {
-      await executeQuery(
-        'INSERT INTO authors_research_papers set ?',
-        author_paper,
-      );
-    });
+    //Exercise 2: Relationships
+    await execQuery(queries.research_Papers.table_creation);
+    await execQuery(queries.authors_researches.table_creation);
+    await execQuery(queries.authors.insertion, [authors]);
+    await execQuery(queries.research_Papers.insertion, [research_Papers]);
+    await execQuery(queries.authors_researches.insertion, [author_papers]);
 
-    const joins_aggregate_functions = await Promise.all([
-      executeQuery(AUTHORS_AND_THERE_MENTORS),
-      executeQuery(AUTHORS_INFORMATION_AND_PAPERS_TITLE),
-      executeQuery(RESEARCHES_AND_NUMBER_OF_AUTHORS),
-      executeQuery(NUMBER_OF_PAPERS_WRITTEN_BY_FEMALES),
-      executeQuery(AVERAGE_INDEX_BY_UNIVERSITY),
-      executeQuery(PAPERS_BY_UNIVERSITY),
-      executeQuery(MIN_MAX_INDEX_BY_UNIVERSITY),
-    ]);
-    console.log(joins_aggregate_functions);
-  } catch (error) {
-    console.log(error.message);
+    // Exercise 3: Joins
+    const author_mentor = await execQuery(queries.joins.join_authors_mentors);
+    const author_research_paper = await execQuery(
+      queries.joins.join_authors_paper_title,
+    );
+    console.log(author_mentor, author_research_paper);
+
+    // Exercise 4: Aggregate Functions
+    const research_papers_count = await execQuery(
+      queries.aggregation.aggregate_papers_authors,
+    );
+    const females_researches = await execQuery(
+      queries.aggregation.aggregate_papers_female,
+    );
+    const avg_h_index = await execQuery(
+      queries.aggregation.aggregate_hIndex_university,
+    );
+    const papers_per_university = await execQuery(
+      queries.aggregation.aggregate_papers_university,
+    );
+    const min_max = await execQuery(
+      queries.aggregation.aggregate_min_max_hIndex,
+    );
+    console.log(
+      research_papers_count,
+      females_researches,
+      avg_h_index,
+      papers_per_university,
+      min_max,
+    );
+  } catch (err) {
+    console.log(err.message);
   }
-})();
+  connection.end();
+};
+
+seedDatabase();
--- Week2/homework/query.js
+++ Week2/homework/query.js
@@ -1,48 +1,65 @@
-const CREATE_TABLE_AUTHORS = `CREATE TABLE authors (author_no INT PRIMARY KEY NOT NULL,
-  author_name VARCHAR(50), university VARCHAR(50), date_of_birth DATE, h_index INT, gender ENUM('M', 'F'));`;
-
-const ADD_MENTOR_COLUMN = `ALTER TABLE authors ADD mentor INT, 
-ADD CONSTRAINT FOREIGN KEY(mentor) REFERENCES authors(author_no);`;
-
-const CREATE_TABLE_RESEARCH_PAPERS = `CREATE TABLE IF NOT EXISTS research_Papers 
-(paper_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
-  paper_title VARCHAR(200), 
-  conference VARCHAR(200), 
-  publish_date DATE);`;
-
-const CREATE_TABLE_AUTHORS_RESEARCH_PAPERS = `CREATE TABLE IF NOT EXISTS authors_research_Papers (
-  author_no INT NOT NULL,
-  paper_id INT NOT NULL,
-  FOREIGN KEY(author_no) REFERENCES authors(author_no), 
-  FOREIGN KEY(paper_id) REFERENCES research_Papers(paper_id)
-    );`;
-
-const AUTHORS_AND_THERE_MENTORS = `SELECT a.author_name, b.author_name as mentor FROM authors AS a JOIN authors b ON a.mentor = b.author_no;`;
-
-const AUTHORS_INFORMATION_AND_PAPERS_TITLE = `SELECT a.* , b.paper_title FROM authors AS a LEFT JOIN authors_research_papers ON a.author_no = authors_research_papers.author_no LEFT JOIN research_papers AS b ON authors_research_papers.paper_id = b.paper_id;`;
-
-const RESEARCHES_AND_NUMBER_OF_AUTHORS = `SELECT a.paper_title, count(author_no) AS num_of_authors FROM research_papers AS a LEFT JOIN authors_research_papers as b ON a.paper_id = b.paper_id GROUP BY paper_title ;`;
-
-const NUMBER_OF_PAPERS_WRITTEN_BY_FEMALES = `SELECT COUNT(paper_id) AS papers_written_by_females FROM authors AS a JOIN authors_research_papers AS b ON a.author_no = b.author_no WHERE gender = "F";`;
-
-const AVERAGE_INDEX_BY_UNIVERSITY = `SELECT university, AVG(h_index) FROM authors GROUP BY university;`;
-
-const PAPERS_BY_UNIVERSITY = `SELECT university, COUNT(a.paper_id) FROM research_papers AS a LEFT JOIN authors_research_papers ON a.paper_id = authors_research_papers.paper_id LEFT JOIN authors AS b ON authors_research_papers.author_no = b.author_no GROUP BY university;`;
-
-const MIN_MAX_INDEX_BY_UNIVERSITY = `SELECT university, min(h_index) , max(h_index)
-FROM authors
-GROUP BY university;`;
-
-module.exports = {
-  CREATE_TABLE_AUTHORS,
-  ADD_MENTOR_COLUMN,
-  CREATE_TABLE_RESEARCH_PAPERS,
-  CREATE_TABLE_AUTHORS_RESEARCH_PAPERS,
-  AUTHORS_AND_THERE_MENTORS,
-  AUTHORS_INFORMATION_AND_PAPERS_TITLE,
-  RESEARCHES_AND_NUMBER_OF_AUTHORS,
-  NUMBER_OF_PAPERS_WRITTEN_BY_FEMALES,
-  AVERAGE_INDEX_BY_UNIVERSITY,
-  PAPERS_BY_UNIVERSITY,
-  MIN_MAX_INDEX_BY_UNIVERSITY,
+const queries = {
+  authors: {
+    table_creation: `CREATE TABLE IF NOT EXISTS authors 
+      (author_no INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+        author_name VARCHAR(200), 
+        university VARCHAR(200), 
+        date_of_birth DATE, 
+        h_index INT, 
+        gender ENUM('M', 'F'))`,
+    insertion: `INSERT INTO authors VALUES ?`,
+    add_mentor: `ALTER TABLE authors ADD mentor INT;`,
+  },
+  research_Papers: {
+    table_creation: `CREATE TABLE research_Papers(paper_id INT PRIMARY KEY,
+           paper_title VARCHAR(200), 
+           conference VARCHAR(150), 
+           publish_date DATE);`,
+    insertion: `INSERT INTO research_Papers VALUES ?`,
+  },
+  authors_researches: {
+    table_creation: `CREATE TABLE authors_researches(id INT PRIMARY KEY AUTO_INCREMENT, 
+          author_no INT NOT NULL, 
+          paper_no INT NOT NULL, 
+          FOREIGN KEY(author_no) REFERENCES authors(author_no), 
+          FOREIGN KEY(paper_no) REFERENCES research_Papers(paper_id));`,
+    insertion: `INSERT INTO authors_researches(author_no, paper_no) VALUES ?`,
+  },
+  joins: {
+    join_authors_mentors: `SELECT authors1.author_name AS author, 
+      authors2.author_name AS mentor 
+      FROM authors authors1 
+      LEFT JOIN authors authors2 
+      ON authors1.mentor = authors2.author_no;`,
+    join_authors_paper_title: `WITH new_query AS (
+      SELECT authors_researches.* , research_papers.paper_title AS research
+      FROM authors_researches
+      JOIN research_papers
+      ON authors_researches.paper_no = research_papers.paper_id)
+      SELECT authors.*, new_query.research
+      FROM authors
+      LEFT JOIN new_query
+      ON authors.author_no = new_query.author_no;`,
+  },
+  aggregation: {
+    aggregate_papers_authors: `SELECT paper_title, COUNT(author_no) AS authors_count
+      FROM research_Papers JOIN authors_researches 
+      ON paper_id = paper_no 
+      GROUP BY paper_title;`,
+    aggregate_papers_female: `SELECT count(id) FROM authors_researches
+      LEFT JOIN authors 
+      ON authors_researches.author_no = authors.author_no
+      WHERE gender = 'F';`,
+    aggregate_hIndex_university: `SELECT university, AVG(h_index) AS average 
+      FROM authors
+      GROUP BY university;`,
+    aggregate_papers_university: `SELECT university, COUNT(paper_no) AS papers_university
+      FROM authors, authors_researches 
+      WHERE authors.author_no = authors_researches.author_no 
+      GROUP BY university;`,
+    aggregate_min_max_hIndex: `SELECT university, MIN(h_index), MAX(h_index) 
+      FROM authors GROUP BY university;`,
+  },
 };
+
+module.exports = queries;
